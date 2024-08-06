f6ad5bcdf763ce98664cf9bd8c6af4d695bf84b1
dmytro.holovnia <dmytro.holovnia@gmail.com>
2024-08-06 15:17:21 +0300

    init commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+
+    public Calculator() {
+
+    }
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int square(int number) {
+        return number * number;
+    }
+ }
---
+++ src/test/java/DefaultTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DefaultTests {
+
+    private static final Calculator calculator = new Calculator();
+
+    @DisplayName("Positive tests")
+    @ParameterizedTest
+    @CsvSource({"4, 2, 2", "7, 5, 2"})
+    public void someTest(int expected, int firstNumber, int secondNumber) {
+        assertEquals(expected, calculator.sum(firstNumber, secondNumber));
+    }
+
+    @DisplayName("Negative test")
+    @Test
+    public void negativeTest() {
+        assertEquals(1, calculator.square(3));
+    }
+
+}
